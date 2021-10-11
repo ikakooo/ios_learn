6ed33e1b3f14864a8db4ca9281062eb24ac5afe7
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-10-11 15:14:03 +0400

    homeWork Started

---
+++ Task_13_FoodApp/Task_13_FoodAppUITests/Task_13_FoodAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Task_13_FoodAppUITestsLaunchTests.swift
+//  Task_13_FoodAppUITests
+//
+//  Created by MacBook Pro on 11.10.21.
+//
+
+import XCTest
+
+class Task_13_FoodAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
